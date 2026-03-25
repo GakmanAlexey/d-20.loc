1e274b5e97718e38c5caf7c77c9eb6cf4d765663
GakmanAlexey <gakman@ya.ru>
2026-03-25 12:28:41 +0300

    0.2.23 Добавлен загрузчик конфигураций для пользовательских данных

---
+++ modules/user/modul/support/config.php
@@ -0,0 +1,45 @@
+<?php
+namespace Modules\User\Modul\Support;
+
+class Config
+{
+    private static $statusLoad = false;
+    public static $config;
+
+    public function __construct()
+    {
+        $configPath = APP_ROOT . DS . "modules" . DS . "user" . DS . "modul" . DS .  "support" . DS ."config.json";
+        self::$config = json_decode(file_get_contents($configPath), true);
+        self::$statusLoad = true;
+    }
+
+    public static function takeFull(){
+        return self::$config;
+    }
+
+    /**
+    * Получение значения по ключу с поддержкой вложенности через точку
+    * Пример: get('limits.min_pass')
+    * 
+    */
+    public static function get($key, $default = null)
+    {
+        $config = self::takeFull();
+        
+        if ($config === null) {
+            return $default;
+        }
+        
+        $keys = explode('.', $key);
+        $value = $config;
+        
+        foreach ($keys as $segment) {
+            if (!isset($value[$segment])) {
+                return $default;
+            }
+            $value = $value[$segment];
+        }
+        
+        return $value;
+    }
+}
